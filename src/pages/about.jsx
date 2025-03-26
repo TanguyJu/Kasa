53a083e8bde136abb2af0ff1e1ed5ee6d3d56b87
tanguyju43@gmail.com <tanguyju43@gmail.com>
2025-03-26 16:22:01 +0100

    Change the banner's design

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -6,7 +6,10 @@ import "../styles/about.scss";
 export default function About() {
   return (
     <div className="about">
+      <div className="about-banner">
       <Banner image={bannerImage} />
+      </div>
+      <div className="about-collapse">
       <Collapse title="Fiabilité">
       Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.
       </Collapse>
@@ -19,6 +22,7 @@ export default function About() {
       <Collapse title="Sécurité">
       La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.
       </Collapse>
+      </div>
     </div>
   );
 }
